1438771afdc6f2547843cbd1b1e8ae0c2ae7d7ae
p-prompang5117 <63015117@kmitl.ac.th>
2022-09-21 12:09:12 +0700

    Google images scraper 092122
    Google image scraper is now online, although might need a bit of tune up for  performance ( also we used cats for scraping examples )

---
+++ Herbis Universalis/gimage_scraper/working_scraper.py
@@ -0,0 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import requests
+import io
+from PIL import Image
+import time
+import bs4
+
+# This program aint need to install chrome-driver (or anything similar)
+'''
+# selenium 3
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
+from selenium.webdriver.common.by import By
+'''
+
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+wd = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+
+def get_images_from_google(wd, delay, max_images):
+    def scroll_down(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(delay)
+
+    url = "https://www.google.com/search?q=cats&tbm=isch&ved=2ahUKEwjykJ779tbzAhXhgnIEHSVQBksQ2-cCegQIABAA&oq=cats&gs_lcp=CgNpbWcQAzIHCAAQsQMQQzIHCAAQsQMQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzIECAAQQzoHCCMQ7wMQJ1C_31NYvOJTYPbjU2gCcAB4AIABa4gBzQSSAQMzLjOYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=7vZuYfLhOeGFytMPpaCZ2AQ&bih=817&biw=1707&rlz=1C1CHBF_enCA918CA918"
+    wd.get(url)
+
+    image_urls = set()
+    skips = 0
+
+    for i in range(0, 6):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1.5)
+
+
+    while len(image_urls) + skips < max_images:
+
+        thumbnails = wd.find_elements(By.CLASS_NAME, "Q4LuWd")
+
+        for img in thumbnails[len(image_urls) + skips:max_images]:
+            try:
+                img.click()
+                time.sleep(delay)
+            except:
+                continue
+
+            images = wd.find_elements(By.CLASS_NAME, "n3VNCb")
+            for image in images:
+                if image.get_attribute('src') in image_urls:
+                    max_images += 1
+                    skips += 1
+                    break
+
+                if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+                    image_urls.add(image.get_attribute('src'))
+                    print(f"Found {len(image_urls)}")
+
+    return image_urls
+
+
+def download_image(download_path, url, file_name):
+    try:
+        image_content = requests.get(url).content
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file)
+        file_path = download_path + file_name
+
+        with open(file_path, "wb") as f:
+            image.save(f, "JPEG")
+
+        print("Downloading Success")
+    except Exception as e:
+        print('FAILED -', e)
+
+
+#---main---#
+urls = get_images_from_google(wd, 5, 100) # webdriver,delay(sec),max_img
+for i, url in enumerate(urls):
+    download_image("scraped_images/", url, str(i) + ".jpg")
+wd.quit()
