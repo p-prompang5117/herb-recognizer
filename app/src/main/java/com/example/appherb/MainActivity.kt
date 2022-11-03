5a280ddf143b6807896484f9ef22c05b317c251e
Sarin Hongthong <sarin120@hotmail.com>
2022-11-03 20:57:08 +0700

    version 3 add Framelayout and Link page with Buttonnavbar

--- app/src/main/java/com/example/appherb/MainActivity.kt
+++ app/src/main/java/com/example/appherb/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.appherb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.appherb.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUptabbar()
+
+    }
+    private fun setUptabbar(){
+        binding.menuBottom.setItemSelected(R.id.home)
+        val homeFragment = home()
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout,homeFragment).commit()
+        binding.menuBottom.setOnItemSelectedListener {
+            when(it){
+                R.id.home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout,homeFragment)
+                        .commit()
+                }
+                R.id.camera -> {
+                    val cameraFragment = camera()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout,cameraFragment)
+                        .commit()
+                }
+                R.id.archive -> {
+                    val archiveFragment = archive()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout,archiveFragment)
+                        .commit()
+                }
+                R.id.settings -> {
+                    val settingFragment = setting()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout,settingFragment)
+                        .commit()
+                }
+
+                else -> {
+
+                }
+            }
+        }
     }
 }
