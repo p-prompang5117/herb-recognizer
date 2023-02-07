90535127405785b362f5af584f45a1c702a08cf8
Sarin Hongthong <sarin120@hotmail.com>
2023-02-07 09:04:57 +0700

    Update brightness and contrast
    เพิ่มและลด ความสว่างและความชัดของรูปภาพในโปรแกรม Augmentation

--- Herbis Universalis/gimage_scraper/pic-alternater.py
+++ Herbis Universalis/gimage_scraper/pic-alternater.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import os
 
 parentlist = os.listdir('scraped_images/')
@@ -20,8 +20,35 @@ for i in parentlist :
             ninety_flipped = imageObject.rotate(90)
             ninety_flipped.save("scraped_images/"+i+"/deg90_"+j)
 
-            grayscaled = imageObject.convert('L')
-            grayscaled.save("scraped_images/"+i+"/grayed_"+j)
+            # image brightness
+            img_enhancer = ImageEnhance.Brightness(imageObject)
+
+            factor = 1
+            enhanced_output = img_enhancer.enhance(factor)
+            enhanced_output.save("scraped_images/" + i + "/b1_" + j)
+
+            factor = 0.5
+            enhanced_output = img_enhancer.enhance(factor)
+            enhanced_output.save("scraped_images/" + i + "/b0.5_" + j)
+
+            factor = 1.5
+            enhanced_output = img_enhancer.enhance(factor)
+            enhanced_output.save("scraped_images/" + i + "/b1.5_" + j)
+
+            # image constrast
+            enhancer = ImageEnhance.Contrast(imageObject)
+
+            factor = 1  # gives original image
+            im_output = enhancer.enhance(factor)
+            im_output.save("scraped_images/" + i + "/c1_" + j)
+
+            factor = 0.5  # decrease constrast
+            im_output = enhancer.enhance(factor)
+            im_output.save("scraped_images/" + i + "/c0.5_" + j)
+
+            factor = 1.5  # increase contrast
+            im_output = enhancer.enhance(factor)
+            im_output.save("scraped_images/" + i + "/c1.5_" + j)
 
         except:
             print("not a pic")
